746736825f3c3c89d49820e2530d0d08982f346e
secolinsky <mticas@taxtechsgv.accountant>
2020-08-06 20:37:35 -0700

    Add express index.js

---
+++ server/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var path = require('path')
+
+// assign port 8080
+app.listen( 8080, () => console.log("server started on port 8080") );
+
+// serve static files
+app.use('/static', express.static(path.resolve(__dirname, "../dist")));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
+})

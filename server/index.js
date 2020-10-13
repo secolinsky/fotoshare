07fd85ce91fbef6439a620c400b9c4c5f56468bc
secolinsky <mticas@taxtechsgv.accountant>
2020-10-12 17:29:44 -0700

    Match server/index.js with client and nodemon branches

--- server/index.js
+++ server/index.js
@@ -2,12 +2,17 @@ var express = require('express');
 var app = express();
 var path = require('path')
 
+// add webpack-dev-middleware
+var webpack = require('webpack');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackConfig = require('./../webpack.dev.js');
+var compiler = webpack(webpackConfig);
+
+app.use(require("webpack-dev-middleware")(compiler, {
+  noInfo: true, publicPath: webpackConfig.output.publicPath
+}));
+
+app.use(require("webpack-hot-middleware")(compiler));
+
 // assign port 8080
-app.listen( 8080, () => console.log("server started on port 8080") );
-
-// serve static files
-app.use('/static', express.static(path.resolve(__dirname, "../dist")));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
-  })
+app.listen( 8080, () => console.log("server started on port 8080") )
